2cbfcd026950c7f5dbcb19b2865a5bc43bfdbbc2
Sergei O. Udalov <sergei.udalov@gmail.com>
2017-08-23 13:30:15 +0300

    gant wip

--- app/controllers/daily_report_controller.rb
+++ app/controllers/daily_report_controller.rb
@@ -3,8 +3,10 @@ require 'jira-ruby'
 class DailyReportController < ApplicationController
   before_action :check_authentication
 
+  FILTER_ID = 25807
+
   def show
-    work_in_progress = jira_client.Filter.find 25807
+    work_in_progress = jira_client.Filter.find FILTER_ID
 
     @issues = work_in_progress.issues.map { |issue| Issue.new(issue) }
   end
---
+++ app/controllers/gantt_controller.rb
@@ -0,0 +1,20 @@
+class GanttController < ApplicationController
+
+
+  def show
+    @issues = cached_issues.reject { |is| is.remainig_working_days == 0 }
+  end
+
+  private
+
+
+  def cached_issues
+    Rails.cache.fetch 'GanttController#issues_data.4' do
+      work_in_progress = jira_client.Filter.find DailyReportController::FILTER_ID
+      new_issues = jira_client.Filter.find BacklogController::FILTER_ID
+
+      (work_in_progress.issues + new_issues.issues).map { |issue| Issue.new(issue) }.map(&:to_hash)
+    end.map { |el| OpenStruct.new(el) }
+  end
+
+end
